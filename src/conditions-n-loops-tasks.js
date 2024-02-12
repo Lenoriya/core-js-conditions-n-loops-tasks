5d77953b646f2271d71e467cd9859fe1a1aaa04b
Helen Kruminia <lena_kru@tut.by>
2024-02-12 21:08:44 +0300

    feat: sortByAsc should return a sorted array

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -383,8 +383,19 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const a = arr;
+  let mid = 0;
+  for (let i = 0; i < a.length - 1; i += 1) {
+    for (let j = i + 1; j < a.length; j += 1) {
+      if (a[i] > a[j]) {
+        mid = a[i];
+        a[i] = a[j];
+        a[j] = mid;
+      }
+    }
+  }
+  return a;
 }
 
 /**
